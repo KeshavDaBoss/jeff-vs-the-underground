a35e24a7977d44042c862084a2e6914b7f41395e
Pratham Yadav <keshavprathamyadav@gmail.com>
2026-05-01 08:53:13 +0530

    Stuff changed

--- scripts/jeff.gd
+++ scripts/jeff.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -900.0
+const JUMP_VELOCITY = -1200.0
 
 
 func _physics_process(delta: float) -> void:
@@ -21,8 +21,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		jump_sound.play()
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
 	if direction:
